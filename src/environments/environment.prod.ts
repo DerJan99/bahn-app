507ca208ff2905da0f79d2c3e93cfec3ad6df4c2
Jan von Sondern <j.n.vonsondern@gmail.com>
2022-05-10 08:20:29 +0200

    added environment vars

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiToken: "0a268a3cae2d9deb5fb3d81b17709fac"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiToken: "2250f59d6db8e8c30784f7373f82d683"
 };
 
 /*
