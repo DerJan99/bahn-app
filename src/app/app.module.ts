1f06eb872ff9a2f7afd87917b0210fd896fe4e04
Jan von Sondern <j.n.vonsondern@gmail.com>
2022-05-10 12:18:20 +0200

    Added Routes to AppRouting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,25 +5,22 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { DashboardModule } from "./components/dashboard/dashboard.module";
 import { StationModule } from "./components/station/station.module";
 
-// TODO: Add dashboard module
-import { ToolbarComponent } from './components/dashboard/toolbar/toolbar.component';
-
-// TODO: Add material module
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatIconModule } from "@angular/material/icon";
 
 @NgModule({
   declarations: [
     AppComponent,
-    ToolbarComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
 
+    DashboardModule,
     StationModule,
 
     MatToolbarModule,
