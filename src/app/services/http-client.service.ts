28e7eaa385900324b1f79484be06df8e9ee2a4e6
Jan von Sondern <j.n.vonsondern@gmail.com>
2022-05-19 09:45:15 +0200

    added station-request from StaDa-api

--- src/app/services/http-client.service.ts
+++ src/app/services/http-client.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import {environment} from "../../environments/environment";
+import {HttpClient} from "@angular/common/http";
+import {ApiParams} from "../interfaces";
 
 @Injectable({
   providedIn: 'root'
@@ -8,5 +10,27 @@ export class HttpClientService {
   private API_URL = "https://api.deutschebahn.com/stada/v2";
   private API_KEY = environment.apiToken;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public getStation(apiParams: ApiParams) {
+    return this.http.get(`${this.API_URL}/stations`, {
+      headers: {
+        'Authorization': `Bearer ${this.API_KEY}`
+      },
+      params: {
+        ...apiParams
+      }
+    });
+  }
+
+  public getStations(limit: number) {
+    return this.http.get(`${this.API_URL}/stations`, {
+      headers: {
+        'Authorization': `Bearer ${this.API_KEY}`
+      },
+      params: {
+        limit: limit
+      }
+    });
+  }
 }
